8343b0f49a8d63f4efb60571b60c99446d8f332c
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-04-29 09:15:02 +0200

    uppstart

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config(); // Laddar in .env
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Lagrar port och uri från env i variabler
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("MONGO_URI").then(()=> {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("Problem with connecting to database" + error);
+});
